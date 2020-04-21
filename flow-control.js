7406386eb794bec574b06061139d8aa561d37c00
Dmitry Sychev <axiumone@gmail.com>
2020-04-21 01:18:25 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,22 +27,31 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  var age = 19
-switch(age >= 13 && age <= 19) {
-  case 1:
-    console.log("You are a teenager");
-    break;
-  case 14:
-    console.log("You are a teenager");
-    break;
-  case 15:
-    console.log("You are a teenager");
-    break;
-  case 16:
-    console.log("You are a teenager");
-    break;
-  default:
-    console.log("You have an age");
-}
+  var age = "19"
+  switch(age) {
+    case "13":
+      console.log("You are a teenager");
+      break;
+    case "14":
+      console.log("You are a teenager");
+      break;
+    case "15":
+      console.log("You are a teenager");
+      break;
+    case "16":
+      console.log("You are a teenager");
+      break;
+    case "17":
+      console.log("You are a teenager");
+      break;
+    case "18":
+      console.log("You are a teenager");
+      break;
+    case "19":
+      console.log("You are a teenager");
+      break;
+    default:
+      console.log("You have an age");
+  }
 
 }
